402ee429b4e5e5544552972c19433e3b655c4c82
Nolan Meyer <100235194+Nolasaurus@users.noreply.github.com>
2023-08-08 16:08:13 -0500

    enable camera control functionality

--- Assets/CameraController.cs
+++ Assets/CameraController.cs
@@ -2,11 +2,13 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    public float zoomSpeed = 10.0f; // Speed of zoom
-    public float minZoom = 2.0f;    // Minimum zoom level
-    public float maxZoom = 10.0f;   // Maximum zoom level
+    public float zoomSpeed = 10.0f;   // Speed of zoom
+    public float minZoom = 2.0f;      // Minimum zoom level
+    public float maxZoom = 10.0f;     // Maximum zoom level
+    public float moveSpeed = 5.0f;    // Speed of movement
+    public float dragSensitivity = 0.01f; // Sensitivity of dragging
 
-    public float moveSpeed = 5.0f;  // Speed of movement
+    private Vector3 dragStart;        // Position of cursor when drag starts
 
     void Update()
     {
@@ -20,5 +22,17 @@ public class CameraController : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
         Vector3 move = new Vector3(horizontalInput, verticalInput, 0) * moveSpeed * Time.deltaTime;
         transform.position += move;
+
+        // Handle dragging with scroll wheel (mouse button 2)
+        if (Input.GetMouseButtonDown(2))
+        {
+            dragStart = Input.mousePosition;
+        }
+        if (Input.GetMouseButton(2))
+        {
+            Vector3 direction = dragStart - Input.mousePosition;
+            Camera.main.transform.position += direction * dragSensitivity;
+            dragStart = Input.mousePosition;
+        }
     }
 }
